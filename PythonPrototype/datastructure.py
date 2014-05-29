b85a0b2bcae864a946fe4ae2431bd31191c5d23b
PatRuediger <pat.ruediger@gmail.com>
2014-05-28 20:03:49 -0700

    First Draw of the Extrapolation
    Still error in Coordinate Transformation, espacially for Vector Fields

---
+++ PythonPrototype/datastructure.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 12 10:46:55 2014
+
+@author: Patrick
+
+Data structure for Visualization
+Using hexahedron Grid cells with 8 vertices
+"""
+import math
+
+
+def mainTest():
+    print('Main startet')
+    DS = AvsUcdAscii()
+    DS.loadFile('C:\out.1550.inp')
+
+def testInterpolation():
+    v0 = Vertex(0,Point3D(0,0,0),Point3D(0,0,0))
+    v1 = Vertex(1,Point3D(0,1,0),Point3D(0,1,0))
+    v2 = Vertex(2,Point3D(1,1,0),Point3D(1,1,0))
+    v3 = Vertex(3,Point3D(1,0,0),Point3D(1,0,0))
+    v4 = Vertex(4,Point3D(0,0,1),Point3D(0,0,1))
+    v5 = Vertex(5,Point3D(0,1,1),Point3D(0,0,1))
+    v6 = Vertex(6,Point3D(1,1,1),Point3D(1,1,1))
+    v7 = Vertex(7,Point3D(1,1,0),Point3D(1,1,0))
+
+    cell = Cell(0,v0,v1,v2,v3,v4,v5,v6,v7)
+    p= [0.5,0.5,0.5]
+    output = cell.isInside(p)
+    print(output)    
+    temp = cell.trilinear(p)
+    output = [temp._x,temp._y, temp._z]
+    print(output)
+    
+class Point3D:
+#    _x = 0.0
+#    _y = 0.0
+#    _z = 0.0
+    
+    def _length(self):
+        return math.sqrt(self._x**2+self._y**2+self._z**2)
+
+    def __init__(self,x,y,z):
+        self._x = x
+        self._y = y
+        self._z = z
+    
+    def mult(self,scalar):
+        return Point3D(self._x * scalar,self._y * scalar,self._z * scalar)
+        
+    def add(self,vec):
+        return Point3D(self._x + vec._x, self._y + vec._y, self._z + vec._z)
+    
+    def __repr__(self):
+        return "("+str(self._x)+","+str(self._y)+","+str(self._z)+")"
+        
+class Vertex:
+#---class Attributes should only be readed
+    #Vertex ID
+    _ID = 0
+    #Postion
+    _pos = Point3D(0,0,0)
+    
+    #Magnetic field
+    _mag = Point3D(0,0,0)
+    #Pointer to nex Vertex
+    _nextVertex = None
+    
+#---class Methods
+    # Constructor method
+    def __init__(self,ID,pos,mag):
+        self._ID = ID        
+        self._pos = pos
+        self._mag = mag
+        
+    def addVertex(self,ID,pos,mag):
+        newVertex = Vertex(ID,pos,mag)
+        self._nextVertex = newVertex
+        return self._nextVertex
+       
+        
+        
+#---- Cell is ment to be an hexahedron   
+class Cell:
+#---class Attributes should only be readed  
+    _ID = 0
+    # should be 8 everytime
+    # ordering: c000,c001,c101,c100,c010,c011,c111,c110
+    _verts =[]    
+    #Pointer to next Cell
+    _nextCell = None
+    
+#---class Methods
+    # Constructor method
+    def __init__(self,ID,v0,v1,v2,v3,v4,v5,v6,v7):
+        self._verts = [v0,v1,v2,v3,v4,v5,v6,v7]
+        self._ID = ID
+        
+    def addCell(self,ID,v0,v1,v2,v3,v4,v5,v6,v7):
+        newCell = Cell(ID,v0,v1,v2,v3,v4,v5,v6,v7)
+        self._nextCell = newCell
+        return self._nextCell
+
+
+    ## @input is an array  with 3 entries
+    def isInside(self,p):
+        bounds = self.boundaries()
+        if bounds[0]<=p[0] and bounds[1]<=p[1] and bounds[2]<=p[2] and bounds[3]>=p[0] and bounds[4]>=p[1] and bounds[5]>=p[2]:
+            return True
+        else:
+            return False
+    
+   
+    ## @input is an array  with 3 entries
+    ## @output is a Point3D
+    def trilinear(self,x):
+        if(self.isInside(x)):
+            xd = (x[0]-self._verts[0]._pos._x)/(self._verts[6]._pos._x-self._verts[0]._pos._x)
+            yd = (x[1]-self._verts[0]._pos._y)/(self._verts[6]._pos._y-self._verts[0]._pos._y)
+            zd = (x[2]-self._verts[0]._pos._z)/(self._verts[6]._pos._z-self._verts[0]._pos._z) 
+            
+            c00 =  Point3D(0,0,0)
+            c10 =  Point3D(0,0,0)
+            c01 =  Point3D(0,0,0)
+            c11 =  Point3D(0,0,0)
+            c0 =  Point3D(0,0,0)
+            c1 =  Point3D(0,0,0)
+            c =  Point3D(0,0,0)
+            
+            c00 = self._verts[0]._mag.mult(1.0-xd)
+            c00.add(self._verts[3]._mag.mult(xd))
+            c10 = self._verts[4]._mag.mult(1.0-xd)
+            c10.add(self._verts[7]._mag.mult(xd))
+            c01 = self._verts[1]._mag.mult(1.0-xd)
+            c01.add(self._verts[2]._mag.mult(xd))
+            c11 = self._verts[5]._mag.mult(1.0-xd)
+            c11.add(self._verts[6]._mag.mult(xd))
+            
+            c0= c00.mult(1.0-yd)
+            c0.add(c10.mult(yd))
+            c1=c01.mult(1.0-yd)
+            c1.add(c11.mult(yd))
+            
+            c = c0.mult(1.0-zd)
+            c.add(c1.mult(zd))
+            return c
+        elif(self._nextCell is not None):
+            self._nextCell.trilinear(x)
+        else:
+            return None
+            
+    def boundaries(self):
+        min_x = 10000000000000000.0
+        min_y = 10000000000000000.0
+        min_z = 10000000000000000.0
+        max_x = 0.0
+        max_y = 0.0
+        max_z = 0.0
+        for v in self._verts:
+            if min_x >= v._pos._x:
+                min_x = v._pos._x
+            if min_y >= v._pos._y:
+                min_y = v._pos._y
+            if min_z >= v._pos._z:
+                min_z = v._pos._z
+            if max_x <= v._pos._x:
+                max_x = v._pos._x
+            if max_y <= v._pos._y:
+                max_y = v._pos._y
+            if max_z <= v._pos._z:
+                max_z = v._pos._z
+        bounds = [min_x,min_y,min_z,max_x,max_y,max_z]
+        #print(bounds)
+        return bounds
+                
+        
+class AvsUcdAscii:
+#---class Attributes should only be readed  
+    _firstVertex = None
+    _lastVertex = None
+    _firstCell = None
+    _lastCell = None
+    _numVert =0
+    _numCells = 0
+    _dim = 0
+    _vertexList =[]
+    _cellList =[]
+#---class Methods
+    # Constructor method
+#    def __init__(self):
+#          return
+    ## returns the interpolated value at position x      
+    def getValue(self,x):
+        if self._firstCell is not None:
+            return self._firstCell.trilinear(x)
+            
+    ## read in data file
+    def loadFile(self,path):
+        with open(path,'r') as file:
+            firstline = file.readline()
+            firstentries = firstline.split()
+            self._numVert = int(firstentries[0])
+            self._numCells = int(firstentries[1]) 
+            self._dim = int(firstentries[2])
+            print('#Vertices:'+ str(self._numVert))
+            print('#Cells:'+ str(self._numCells))
+            
+            current = Vertex(0,Point3D(0,0,0),Point3D(0,0,0))
+            currentCell = Cell(0,current,current,current,current,current,current,current,current)
+            ver_counter = 1
+            cell_counter = 1
+            value_counter = 1
+            i = 1
+            for line in file:
+                ### split by whitespaces
+                entries = line.split()
+                if ver_counter is 1:
+                    print('read first Vertex')
+                    ID = int(entries[0])
+                    pos = Point3D(float(entries[1]),float(entries[2]),float(entries[3]))
+                    ## changed later
+                    mag = Point3D(0.0,0.0,0.0)
+                    current =  Vertex(ID,pos,mag)
+                    self._firstVertex = current
+                    self._vertexList.append(current)
+                    ver_counter+=1
+                elif ver_counter <= self._numVert:
+                    if (ver_counter*100/self._numVert)>=(10*i):
+                        print('Vertex loading reached: ' + str(10*i) + '%')
+                        i+=1
+               #     print('read'+str(ver_counter)+'Vertex')
+                    ID = int(entries[0])
+                    pos = Point3D(float(entries[1]),float(entries[2]),float(entries[3]))
+                    ## changed later
+                    mag = Point3D(0.0,0.0,0.0)
+                    #print('before:'+str(current._ID))
+                    nextV = current.addVertex(ID,pos,mag)
+                    self._vertexList.append(nextV)
+                    current = nextV
+                    #print('after: ' +str(current._ID))
+                    ver_counter+=1  
+                elif cell_counter is 1:
+                    i=1
+                    print('read first Cell')
+                    ID = int(entries[0])
+                  #  currentCell = Cell(ID,self.getVertex(entries[3]),self.getVertex(entries[4]),self.getVertex(entries[5]),self.getVertex(entries[6]),self.getVertex(entries[7]),self.getVertex(entries[8]),self.getVertex(entries[9]),self.getVertex(entries[10]))
+                    currentCell = Cell(ID,self._vertexList[int(entries[3])-1],self._vertexList[int(entries[4])-1],self._vertexList[int(entries[5])-1],self._vertexList[int(entries[6])-1],self._vertexList[int(entries[7])-1],self._vertexList[int(entries[8])-1],self._vertexList[int(entries[9])-1],self._vertexList[int(entries[10])-1])
+                    self._firstCell = currentCell
+                    cell_counter +=1
+                elif cell_counter <= self._numCells:
+                    if (cell_counter*100/self._numCells)>=(10*i):
+                        print('Cell loading reached: ' + str(10*i) + '%')
+                        i+=1
+                    ID = int(entries[0])
+                    #nextC= currentCell.addCell(ID,self.getVertex(entries[3]),self.getVertex(entries[4]),self.getVertex(entries[5]),self.getVertex(entries[6]),self.getVertex(entries[7]),self.getVertex(entries[8]),self.getVertex(entries[9]),self.getVertex(entries[10]))
+                    nextC= currentCell.addCell(ID,self._vertexList[int(entries[3])-1],self._vertexList[int(entries[4])-1],self._vertexList[int(entries[5])-1],self._vertexList[int(entries[6])-1],self._vertexList[int(entries[7])-1],self._vertexList[int(entries[8])-1],self._vertexList[int(entries[9])-1],self._vertexList[int(entries[10])-1])
+                    currentCell = nextC
+                    cell_counter +=1    
+                elif value_counter is 1:
+                    ## skip the line
+                    value_counter +=1
+                    i=1
+                elif value_counter is 2:
+                    ##skip the line
+                    value_counter +=1
+                    print('read first value')
+                elif value_counter <=(self._numCells+2):
+                    if (value_counter*100/self._numVert)>=(10*i):
+                        print('Values loading reached: ' + str(10*i) + '%')
+                        i+=1
+                    v = self._vertexList[(int(entries[0]))-1]
+                    v._mag = Point3D(float(entries[1]),float(entries[2]),float(entries[3]))
+                    value_counter +=1
+    
+    ## return Vertex from his ID
+    def getVertex(self,ID):       
+        vertex = self._firstVertex
+        while vertex._ID != int(ID):
+            vertex = vertex._nextVertex
+        return vertex
+
+        
---
+++ PythonPrototype/sphericalharmo.py
@@ -0,0 +1,428 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 15 09:19:17 2014
+
+@author: Patrick
+
+Algortithms for data extraction and Extrapolation
+
+"""
+
+import datastructure
+from datastructure import Point3D
+from datastructure import AvsUcdAscii
+from numpy import *
+import math
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+#from mayavi import mlab
+
+
+
+# List which contains the streamline datapoints of the forward/backward rungeKutta
+fw_points=[]
+bw_points=[]
+
+#List which contains the evaluated datapoints from the SHA
+sha_points=[]
+plotPos_x=[]
+plotPos_z=[]
+plotPos_y=[]
+plotValue_x=[]
+plotValue_y=[]
+plotValue_z=[]
+
+#Gauss coefficients with max degree m
+n=4
+g=ndarray((6,6))
+h=ndarray((6,6))
+#earth radius
+#a = 6371000
+ar= 2.91
+# data Object 
+data = AvsUcdAscii()
+
+# x,y,z should be list of number type objects
+def drawStreamLine(x,y,z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z, c='red')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+plt.show()
+
+
+def drawVectorField(x,y,vx,vy):
+    fig = plt.figure()
+    ax= fig.add_subplot(111)
+    ax.scatter(x,y)
+    ax.quiver(x,y,vx,vy)
+    
+#computes a whole streamline    
+def streamline(start,t_max,step):
+    """
+    @Input:start postion (Point3D), max number of timesteps (Integer), stepsize (float)
+    @Output:Returns a list of tuples, evaluated points from the rk4 method which can be plottet afterwards
+    Last item in the list can be uses as SHA input    
+    """
+    t=0
+    sl=[]
+    vl=[]
+    nextPos = start
+    nextVal = data.getValue(nextPos)
+    while t < t_max:
+        if data.getValue(start) is None:
+            print("Leaving data domain ....... start Extrapolation")
+            sl.append(rk4(nextPos,nextVal,evalSHA,t)[0])
+            vl.append(rk4(nextPos,nextVal,evalSHA,t)[1])
+        else:
+            ## Inside data domain
+            sl.append(rk4(nextPos,nextVal,evalf,t)[0])
+            vl.append(rk4(nextPos,nextVal,evalf,t)[1])
+        nextPos = sl[len(sl)-1]
+        nextVal = vl[len(vl)-1]
+        t+=step
+    return sl
+
+def evalf(x,v,dt):
+    return data.getValue(x)
+
+def evalSHA(x,v,dt):
+    return sphericalHarmoAnalysis(x)    
+    
+def sphericalHarmoAnalysis(x):
+    """Evaluates the spherical harmonics equation for the magnetic field, with degree n
+    @Input: Point3D Position
+    @Output: Point3D Magnetic Field
+    """
+    ##Coord Transformation
+    #print("Pos Cartesian: "+ str(x._x)+","+str(x._y)+","+str(x._z))
+    #print(x)
+    v = toSpherical(x)
+    #print(v)
+    #print("Pos Spherical: "+ str(v._x)+","+str(v._y)+","+str(v._z))
+    result=Point3D(0,0,0)
+
+#    for l in range(1,2):
+#        for m in range(l+1):
+    m = 1
+    l = 1
+    result._x+= -((ar/v._z)**(l +2))*deltaSN(m,l,math.cos(v._x))*(g[l][m]*math.cos(m*v._y)+h[l][m]*math.sin(m*v._y))
+  #  print(-g[l][m],m,math.sin(m*v._y),h[l][m],m,math.cos(m*v._y))
+    result._y+= -(ar/(v._z))**(l +2)*SN(m,l,cos(v._x))*(-g[l][m]*m*math.sin(m*v._y)+h[l][m]*m*math.cos(m*v._y))
+#    result._y+=(-g[l][m]*m*math.sin(m*v._y)+h[l][m]*m*math.cos(m*v._y))
+    result._z+= -(l +1)*((ar/v._z)**(l +2))*SN(m,l,cos(v._x))*(g[l][m]*math.cos(m*v._y)+h[l][m]*math.sin(m*v._y))
+#            print((l+1)*((a/v._z)**(l+2)),SN(m,l,cos(v._x)),g[l][m]*math.cos(m*v._y),h[l][m]*math.sin(m*v._y))
+#            print("sha", l,m,result)
+    """    for l in range(1,n+1):
+        for m in range(l+1):
+            result._x+=((a/v._z)**(l+1))*deltaSN(m,l,math.cos(v._x))*(g[l][m]*math.cos(m*v._y)+h[l][m]*math.sin(m*v._y))
+            #print(a/v._z, (l+1), deltaSN(m,l,math.cos(v._x)), g[l][m], math.cos(m*v._y), h[l][m], math.sin(m*v._y))
+            result._y+=((a/v._z)**(l+1))*SN(m,l,cos(v._x))*(-g[l][m]*m*math.sin(m*v._y)+h[l][m]*m*math.cos(m*v._y))
+            result._z+=(l+1)*((a/v._z)**(l+2))*SN(m,l,cos(v._x))*(g[l][m]*math.cos(m*v._y)+h[l][m]*math.sin(m*v._y))
+            #print(l,m,result)"""
+
+    
+   # print("sha",result)
+    #print(result)
+    #Coord Transformation
+    print("sha,spherical:",result)
+    vf = toCartesianVecfield(v,result)
+    print("sha,cartesian:",vf)
+    #print(v)
+    #print(str(v._x) +"," + str(v._y) +"," +str(v._z))    
+    return vf
+    
+#Schmidt-Normalized Legendrefunction    
+def SN(m,l,x):
+    #print("compute Schmidt- Normalized Legendre " + str(l)+" , " + str(m))
+    return ((-1)**m)*math.sqrt(2.0*math.factorial(l-m)/math.factorial(l+m))*Legendre(m,l,x)
+def deltaSN(m,l,x):
+    #print("compute derivative Schmidt- Normalized Legendre " + str(l)+" , " + str(m))
+    return ((-1)**m)*math.sqrt(2.0*math.factorial(l-m)/math.factorial(l+m))*deltaLegendre(m,l,x)   
+
+
+#Legendre Functions till degree 4    
+def Legendre(m,l,x):
+    if l==1 and m==0:
+        return x
+    elif l==1 and m==1:
+  #      if x**2 >1.0:
+  #          return 0.0
+  #      else: 
+            return math.sqrt(1.0-x**2.0)*(-1.0)
+    elif l==2 and m ==0:
+        return 0.5*(3.0*x**2.0 -1.0)
+    elif l==2 and m==1:
+  #      if x**2 > 1.0:
+  #          return 0.0
+  #      else: 
+            return -3.0*x*math.sqrt(1.0-x**2.0)
+    elif l==2 and m ==2:
+        return 3.0*(x**2.0 -1.0)*(-1.0)
+    elif l==3 and m ==0:
+        return 0.5*(5.0*x**3.0 -3.0*x)
+    elif l==3 and m==1:
+   #     if x**2 >1.0:
+   #         return 0.0
+   #     else: 
+            return(-3.0/2.0)*math.sqrt(1.0-x**2.0)*(5.0*x**2.0 -1.0)
+    elif l==3 and m ==2:
+        return -15.0*x*(x**2.0 -1.0)
+    elif l==3 and m ==3:
+        return -15.0*(1.0-x**2.0)**(3.0/2.0)
+    elif l==4 and m ==0:
+        return (1.0/8.0)*(35.0*x**.04 -30.0*x**2.0 +3.0)
+    elif l==4 and m==1:
+   #     if x**2 >1.0:
+   #         return 0.0
+   #    else: 
+            return -(5.0/2.0)*math.sqrt(1.0-x**2.0)*(7.0**3.0 -3.0*x)
+    elif l==4 and m==2:
+        return -(15.0/2.0)*(x**2.0 -1.0)*(7.0*x**2.0 -1.0)
+    elif l==4 and m==3:
+        return 105.0*x*(1.0-x**2.0)**(3.0/2.0)
+    elif l==4 and m==4:
+        return 105*(x**2.0 -1.0)**2.0
+        
+#First derivative of Legende Functions        
+def deltaLegendre(m,l,x):
+    if l==1 and m == 0:
+        return 1.0
+    elif l==1 and m==1:
+#        if x**2 > 1.0:
+#            return 0.0
+#        else: 
+            return x/math.sqrt(1.0-x**2.0)
+    elif l==2 and m ==0:
+        return 3.0*x
+    elif l==2 and m==1:
+ #       if x**2 >1.0:
+ #           return 0.0
+ #      else: 
+            return (6.0*x**2.0 -3.0)/(math.sqrt(1.0-x**2.0))
+    elif l==2 and m ==2:
+        return -6.0*x
+    elif l==3 and m ==0:
+        return (3.0/2.0) *(5.0*x**2.0 -1.0)
+    elif l==3 and m==1:
+ #       if x**2 > 1.0:
+ #           return 0.0
+ #       else: 
+            return (3.0*x*(15.0*x*2.0 -11.0))/(2.0*math.sqrt(1.0-x**2.0))
+    elif l==3 and m ==2:
+        return 15.0 -45.0*x**2.0
+    elif l==3 and m ==3:
+ #       if x**2 > 1.0:
+ #          return 0.0
+ #       else: 
+            return 45.0*x*math.sqrt(1.0-x**2.0)
+    elif l==4 and m ==0:
+        return (5.0/2.0)*x*(7.0*x**-3.0)
+    elif l==4 and m==1:
+ #       if x**2 > 1.0:
+ #           return 0.0
+ #       else: 
+            return (5.0*(28.0*x**4.0 - 27.0*x**2.0 + 3.0))/(2*math.sqrt(1.0-x**2.0))
+    elif l==4 and m==2:
+        return -30.0*x*(7.0*x**2.0 -4.0)
+    elif l==4 and m==3:
+ #       if (x**2) > 1.0:
+ #           return 0.0
+ #       else:         
+            return 105.0*math.sqrt(1.0-x**2.0)*(4.0*x**2.0 -1.0)
+    elif l==4 and m==4:
+        return 420.0*x*(x**2.0 -1.0)
+        
+#Coordinate transformation Spherical (theta,phi,r)  to Cartesian (x,y,z)
+def toCartesian(x):
+    v=datastructure.Point3D(0,0,0)
+    v._x = x._z*math.sin(x._x)*math.cos(x._y)
+    v._y = x._z*math.sin(x._x)*math.sin(x._y)
+    v._z = x._z*math.cos(x._x)        
+    return v
+#Coordinate transformation Cartesian  to Spherical
+def toSpherical(x):
+    v=datastructure.Point3D(0,0,0)
+    #theta
+    v._x = math.acos(x._z/x._length())
+    #phi
+    v._y = math.atan2(x._y,x._x)
+    #r
+    v._z = x._length()
+    return v
+    
+    
+def toSphericalVecfield(x,v):
+    """ Pos x has to be in spherical coordinates 
+    """
+    vf=datastructure.Point3D(0,0,0)
+    vf._z= v._x*math.sin(x._x)*math.cos(x._y) + v._y*math.sin(x._x)*math.sin(x._y) + v._z*math.cos(x._y)
+    vf._x = v._x*math.cos(x._x)*math.cos(x._y) + v._y*math.cos(x._x)*math.sin(x._y) - v._y*math.sin(x._y)
+    vf._y = -v._x*math.sin(x._y) + v._y*math.cos(x._y) 
+    return vf
+    
+def toCartesianVecfield(x,v):
+    """ Pos x has to be in spherical coordinates 
+    """
+    vf=datastructure.Point3D(0,0,0)
+    vf._x = v._z*math.sin(x._x)*math.cos(x._y) + v._x*math.cos(x._x)*math.cos(x._y)- v._y*math.sin(x._y)
+    vf._y = v._z*math.sin(x._x)*math.sin(x._y) + v._x*math.cos(x._x)*math.sin(x._y)+ v._y*math.cos(x._y)
+    vf._z = v._z*math.cos(x._x) - v._x*math.sin(x._x)     
+    return vf
+    
+#Method for the parrallel SHA to check if the 2 evaluated points are in a certain range
+def rangeCheck(v1,v2):
+    return True
+    
+def rk4(x, v, a, dt):
+    """Returns final (position, magnetic field) tuple after
+    time dt has passed.
+
+    x: initial position (Point3D)
+    v: initial magnetic field (Point3D)
+    a: evaluation fucntion a(x,v,dt) (must be callable) should return a number like object (e.g. trilinear interpolation)
+    dt: timestep (number)"""
+    x1 = datastructure.Point3D(x._x,x._y,x._z)
+    v1 = datastructure.Point3D(v._x,v._y,v._z)
+    a1 = a(x1, v1, 0)
+
+    x2 = x.add(v1.mult(0.5*dt))
+    v2 = v.add(a1.mult(0.5*dt))
+    a2 = a(x2, v2, dt/2.0)
+
+    x3 = x.add(v2.mult(0.5*dt))
+    v3 = v.add(a2.mult(0.5*dt))
+    a3 = a(x3, v3, dt/2.0)
+
+    x4 = x.add(v3.mult(dt))
+    v4 = v.add(a3.mult(dt))
+    a4 = a(x4, v4, dt)
+
+    print(v1, v2, v3, v4)
+    print(a1, a2, a3, a4)
+    xf = x
+    xf=xf.add(v1.mult(dt/6))
+    xf=xf.add(v2.mult(dt/6*2))
+    xf=xf.add(v3.mult(dt/6*2))
+    xf=xf.add(v4)
+#    xf = x + (dt/6.0)*(v1 + 2*v2 + 2*v3 + v4)
+    vf = v.add(a1.mult(dt/6))
+    vf=vf.add(a2.mult(dt/6*2))
+    vf=vf.add(a3.mult(dt/6*2))
+    vf=vf.add(a4)
+#    vf = v + (dt/6.0)*(a1 + 2*a2 + 2*a3 + a4)
+
+    print("Pos: " + str(xf._x)+ ","+ str(xf._y)+ "," + str(xf._z))
+    print("Val: " + str(vf._x)+ ","+ str(vf._y)+ "," + str(vf._z))
+    return xf, vf
+
+def eulerForward(x,v,a,step=1):
+    """Returns final (position, magnetic field,stepsize) triple after
+    time dt has passed.
+    x: initial position (Point3D)
+    v: initial magnetic field (Point3D)
+    a: evaluation fucntion a(x,v,dt) (must be callable) should return a number like object (e.g. trilinear interpolation)
+    step: initial timestep, only usefull, if it takes very long to find the first step (number)"""
+    #error treshold for adaptive stepsize
+    err = 1.0e-4  
+    x0 = datastructure.Point3D(x._x,x._y,x._z)
+    v0 = datastructure.Point3D(v._x,v._y,v._z)
+    
+    dt = step
+    
+    x1=x0.add(v0.mult(dt))
+    v1=a(x,v,dt)
+    x2=x0.add(v0.mult(dt/2))
+    counter =0
+#    print(x1,v1,x2)
+#    v2=a(x,v,dt/2) 
+    #adaptive stepsize
+    while (abs(x1._x-x2._x)>= err) and (abs(x1._y-x2._y)>= err) and (abs(x1._z-x2._z) >= err):  
+        counter+=1
+        dt/=2
+        x1=x0.add(v0.mult(dt))
+        v1=a(x,v,dt)
+        x2=x0.add(v0.mult(dt/2))
+#        v2=a(x,v,dt/2) 
+#    print(counter,dt)
+#    print("Stepsize found")
+#    t2=dt
+#    print(t2,t)
+#    while t2 < t:
+#        x1,v1,t3 = eulerForward(x1,v1,a,dt)
+#        t2+=t3
+#        print(t2,t)
+#        exit()
+    
+    return Point3D(x1._x,x1._y,x1._z),Point3D(v1._x,v1._y,v1._z),dt
+def testSHA(x,y,z,mx,my,mz):
+    loadGaussCoef("D:\Uni\GeodynamicsProject\GausCoef.txt")
+    for theta in range(10,3141,100):
+        tpr = datastructure.Point3D(theta/1000.0, math.pi, 2.91)
+        xyz = toCartesian(tpr)
+        plotPos_x.append(xyz._x)
+        plotPos_y.append(xyz._z)
+        v = evalSHA(xyz, None, None)
+        plotValue_x.append(v._x)
+        plotValue_y.append(v._z)
+        vf = toSphericalVecfield(tpr,v)
+        print(tpr,vf,v)
+        
+    drawVectorField(plotPos_x,plotPos_y,plotValue_x,plotValue_y)
+    return
+    
+    start = datastructure.Point3D(x,y,z)
+    t=0.0
+    sl=[]
+    vl=[]
+    tmax = 0.002
+    #initial stepsize, optional
+    step = 0.001
+    nextPos = start
+    plotPos_x.append(start._x)
+    plotPos_y.append(start._y)
+    plotPos_z.append(start._z)
+    nextVal = datastructure.Point3D(mx,my,mz)
+    while t < tmax:
+        print("t: " + str(t))
+#        xf,vf = rk4(nextPos,nextVal,evalSHA,t)
+        xf, vf ,t2= eulerForward(nextPos,nextVal,evalSHA,step)
+        print( xf)
+        plotPos_x.append(xf._x)
+        plotPos_y.append(xf._y)
+        plotPos_z.append(xf._z)
+        plotValue_x.append(vf._x)
+        plotValue_y.append(vf._y)
+        plotValue_z.append(vf._z)
+        sl.append(xf)
+        vl.append(vf)
+        nextPos = sl[len(sl)-1]
+        nextVal = vl[len(vl)-1]
+        t+=t2
+#        t+=step
+#    drawStreamLine(plotPos_x,plotPos_y,plotPos_z)
+    drawVectorField(plotPos_x,plotPos_y,plotValue_x,plotValue_y)
+        
+
+def loadGaussCoef(filename):
+    with open(filename,'r') as file:
+        count = 0
+        for line in file:
+            if(count<2):
+                ##skip first 2 lines
+                doNothing=0
+                count +=1
+            else:
+                entries = line.split()
+                if entries[0] is 'g':
+                    g[int(entries[1])][int(entries[2])] =float(entries[3])
+                    count +=1
+                elif entries[0] is 'h':
+                    h[int(entries[1])][int(entries[2])] =float(entries[3])
+                    count +=1
+                else :
+                    print("Error: Invalid File Format")
+            
